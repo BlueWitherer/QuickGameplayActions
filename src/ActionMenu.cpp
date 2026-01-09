704dbddc48365adb7c9a15e794ef8538cf0ee833
Cheeseworks <bluewitherer2422@gmail.com>
2026-01-09 13:45:08 -0500

    fix allat

--- src/ActionMenu.cpp
+++ src/ActionMenu.cpp
@@ -63,7 +63,7 @@ bool ActionMenu::init(PlayLayer* pl) {
 
     setContentSize(m_impl->m_sprite->getScaledContentSize());
 
-    m_impl->m_sprite->setPosition({ getScaledContentWidth() / 2.f, (getScaledContentHeight() / 2.f) - 1.25f });
+    m_impl->m_sprite->setPosition(getScaledContentSize() / 2.f);
 
     setScale(m_impl->m_scale); // set initial scale
     setOpacity(m_impl->m_opacity); // set initial opacity
@@ -78,7 +78,7 @@ bool ActionMenu::init(PlayLayer* pl) {
     m_impl->m_menu->setID("actions-menu");
     m_impl->m_menu->setAnchorPoint({ 0, 1 });
     m_impl->m_menu->setContentSize({ 0.f, 25.f });
-    m_impl->m_menu->setPosition({ (getScaledContentWidth() / 2.f) + 10.f, getScaledContentHeight() / 2.f });
+    m_impl->m_menu->setPosition({ (getScaledContentWidth() / 2.f) + 6.25f, (getScaledContentHeight() / 2.f) - 1.25f });
     m_impl->m_menu->setVisible(m_impl->m_show);
     m_impl->m_menu->setLayout(layout);
 
@@ -114,7 +114,7 @@ bool ActionMenu::init(PlayLayer* pl) {
     m_impl->m_menuBg->setScale(0.125f);
     m_impl->m_menuBg->setOpacity(m_impl->m_opacity / 2);
     m_impl->m_menuBg->setAnchorPoint(m_impl->m_menu->getAnchorPoint());
-    m_impl->m_menuBg->setContentSize({ (m_impl->m_menu->getScaledContentWidth() + 20.f) * 8.f, (m_impl->m_menu->getScaledContentHeight() * 8.f) + 2.5f });
+    m_impl->m_menuBg->setContentSize({ (m_impl->m_menu->getScaledContentWidth() + 15.f) * 8.f, (m_impl->m_menu->getScaledContentHeight() * 8.f) + 2.5f });
     m_impl->m_menuBg->setPosition(m_impl->m_sprite->getPosition());
     m_impl->m_menuBg->setVisible(m_impl->m_show);
 
